e70180157c563ebbfb41e695f219e84bff59118d
Noah Wang <ponmonw@gmail.com>
2022-04-19 21:22:20 -0700

    modal for creating a new sspace

--- static/script.js
+++ static/script.js
@@ -1,4 +1,33 @@
 $(document).ready(function(){
+  // Get the modal
+var modal = document.getElementById("create-space");
+
+// Get the button that opens the modal
+var btn = document.getElementById("create");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+
+
+
   var chatDiv = document.getElementById("chat");
   var element = document.getElementById("submit"); //grab the element
   element.onclick = function() { 
