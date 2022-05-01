dbeb7a35307227bb223ce7d07aca87cdcdef8ca2
Ramon Wang <ponmorw@gmail.com>
2022-05-01 13:46:58 -0700

    typing

--- static/script.js
+++ static/script.js
@@ -1,19 +1,8 @@
 $(document).ready(function(){
 
-var createSpaceModal = document.getElementById("create-space");
-var createSpaceButton = document.getElementById("create-space-button");
-var closeSpaceModal = document.getElementById("close-create-space");
-
-//createSpaceButton.onclick = function() {
- // createSpaceModal.style.display = "block";
-//}
-
-
-window.onclick = function(event) {
-  if (event.target == createSpaceModal) {
-    createSpaceModal.style.display = "none";
-  }
-}
+  var createSpaceModal = document.getElementById("create-space");
+  var createSpaceButton = document.getElementById("create-space-button");
+  var closeSpaceModal = document.getElementById("close-create-space");
 
   $(document).on('click', '#create-space-button', function() {
     createSpaceModal.style.display = "block";
@@ -21,13 +10,27 @@ window.onclick = function(event) {
   $(document).on('click', '#close-create-space', function() {
     createSpaceModal.style.display = "none";
   });
+  window.onclick = function(event) {
+    if (event.target == createSpaceModal) {
+      createSpaceModal.style.display = "none";
+    }
+  }
   
-  //$(document).on('click', function(e) {
-  //  if(!(($(e.target).closest("#modalBox").length > 0 ) || ($(e.target).closest("#modal-btn").length > 0))){
-  //  $("#modalBox").hide();
- //  }
- // });
-  
+  //var createRoomModal = document.getElementById("create-room");
+  //var createRoomButton = document.getElementById("create-room-button");
+  //var closeRoomModal = document.getElementById("close-create-room");
+
+  /*$(document).on('click', '#create-space-button', function() {
+    createSpaceModal.style.display = "block";
+  });
+  $(document).on('click', '#close-create-space', function() {
+    createSpaceModal.style.display = "none";
+  });
+  window.onclick = function(event) {
+    if (event.target == createSpaceModal) {
+      createSpaceModal.style.display = "none";
+    }
+  }*/
    
   $("#chat").on('mouseenter', '.message-container-combine', function(){
     $(this).find(".message-combine-time").css("visibility", "visible");
