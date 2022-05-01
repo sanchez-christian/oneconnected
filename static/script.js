45f1eda01f32b4670a3857766643de9ef429a334
Ramon Wang <ponmorw@gmail.com>
2022-05-01 13:13:58 -0700

    typing

--- static/script.js
+++ static/script.js
@@ -1,29 +1,26 @@
 $(document).ready(function(){
-  // Get the modal
-var modal = document.getElementById("create-space");
 
-// Get the button that opens the modal
-var btn = document.getElementById("create");
+var createSpaceModal = document.getElementById("create-space");
+var createSpaceButton = document.getElementById("create-space-button");
+var closeSpaceModal = document.getElementById("close-create-space");
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+//createSpaceButton.onclick = function() {
+ // createSpaceModal.style.display = "block";
+//}
 
-// When the user clicks on the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
+closeSpaceModal.onclick = function() {
+  createSpaceModal.style.display = "none";
 }
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
+  if (event.target == createSpaceModal) {
+    createSpaceModal.style.display = "none";
   }
 }
+
+  $(document).on('click', '#create-space-button', function() {
+    createSpaceModal.style.display = "block";
+  });
 
   $("#chat").on('mouseenter', '.message-container-combine', function(){
     $(this).find(".message-combine-time").css("visibility", "visible");
