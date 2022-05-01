e72870df99225e2989b014c66d1f36d0c6320f7e
Ramon Wang <ponmorw@gmail.com>
2022-05-01 13:26:43 -0700

    typing

--- static/script.js
+++ static/script.js
@@ -8,20 +8,21 @@ var closeSpaceModal = document.getElementById("close-create-space");
  // createSpaceModal.style.display = "block";
 //}
 
-closeSpaceModal.onclick = function() {
-  createSpaceModal.style.display = "none";
-}
 
-window.onclick = function(event) {
-  if (event.target == createSpaceModal) {
-    createSpaceModal.style.display = "none";
-  }
-}
+//window.onclick = function(event) {
+//  if (event.target == createSpaceModal) {
+//    createSpaceModal.style.display = "none";
+//  }
+//}
 
   $(document).on('click', '#create-space-button', function() {
     createSpaceModal.style.display = "block";
   });
-
+  $(document).on('click', '#close-create-space', function() {
+    createSpaceModal.style.display = "none";
+  });
+  $(document).on('click', '#c')
+  
   $("#chat").on('mouseenter', '.message-container-combine', function(){
     $(this).find(".message-combine-time").css("visibility", "visible");
   });
