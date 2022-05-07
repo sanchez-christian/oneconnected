2d580029a5a1b34e4581f1bbcfdb16a24c5d09b4
Ramon Wang <ponmorw@gmail.com>
2022-05-07 11:09:40 -0700

    Alert for finding room down DOM oof channels after deleting

--- webapp.py
+++ webapp.py
@@ -180,8 +180,8 @@ def send_message(data):
     socketio.emit('recieve_message', data, room = data['room'])
     
 @socketio.on('deleted_room')
-def deleted_message(data):
-	socketio.emit('move_room', data, room = data['room'])
+def deleted_message():
+	socketio.emit('move_room', room = data['room'])
 	#data is unnecessary
 
 @app.route('/sbhs')
