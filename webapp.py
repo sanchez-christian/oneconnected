98605ddd08e8530836116ca44f5f8dad782f72a1
Ramon Wang <ponmorw@gmail.com>
2022-05-25 19:37:44 -0700

    Changes May 25, 2022

--- webapp.py
+++ webapp.py
@@ -12,6 +12,7 @@ import requests
 
 from bson.objectid import ObjectId
 from bson.json_util import dumps
+_id = ObjectId()
 
 #import pprint
 #import sys
@@ -342,7 +343,7 @@ def call_route():
         spaces = collection_users.find_one({ "_id": session['unique_id']})['joined']
         spaces_list = []
         for space_item in spaces:
-            spaces_list.append(collection_spaces.find_one(ObjectId({'_id': space_item})))
+            spaces_list.append(collection_spaces.find_one({'_id': ObjectId(space_item)}))
         spaces_list = dumps(spaces_list)
         return Response(spaces_list, mimetype='application/json')
 
