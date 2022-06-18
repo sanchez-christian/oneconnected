534a66c1fee7140c8649b2995a00edfcfa607f98
Ramon Wang <ponmorw@gmail.com>
2022-06-18 15:08:29 -0700

    changes

--- webapp.py
+++ webapp.py
@@ -377,8 +377,11 @@ def delete_message():
         deleted_email = collection_messages.find({'email': deleted_message['email']}).sort('_id', pymongo.DESCENDING)
         document_list = list(deleted_email)
         message_index = document_list.index(deleted_message)
-        #https://www.delftstack.com/howto/python/python-search-list-of-dictionaries/#:~:text=We%20can%20easily%20search%20a,with%20the%20list()%20function.
-        ##change_combine_status = document_list[message_index-1]
+        
+        if message_index != 0:
+            if document_list[message_index-1]["combine"] == "true":
+                collection_messages.find_one_and_update({'_id': document_list[message_index-1]['_id']}, {'$set': {'combine': 'false'}}) 
+                
         #collection_deleted.insert_one({'name': session['users_email'], 'datetime': datetime.now().isoformat() + 'Z', 'deleted_message_content': deleted_message}) Used to add to logs once deleted.
         collection_messages.delete_one({"_id": ObjectId(request.json['message_id'])})
         return Response(dumps({'success': message_index}), mimetype='application/json')
