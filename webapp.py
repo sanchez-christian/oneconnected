4bc91fa0977bd1fc18f91fec7d9d579b1781afde
Ramon Wang <ponmorw@gmail.com>
2022-07-24 21:53:05 -0700

    Changes

--- webapp.py
+++ webapp.py
@@ -504,6 +504,7 @@ def sorted_rooms(data):
             for room in section[1:]:
                 collection_rooms.find_one_and_update({"_id": ObjectId(room)}, {'$set': {'order': order, 'section': section[0]}})
                 order += 1
+                socketio.emit('sorted_rooms', data, room = room)
         order = 1
     socketio.emit('sorted_rooms', data, room = '62d5e94dc2ce65f07f7a8126')
 
