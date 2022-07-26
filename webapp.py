780d63ea467efe81bf3c80a7842e604d71e1a169
Ramon Wang <ponmorw@gmail.com>
2022-07-25 17:18:01 -0700

    July 25, 2022 Changes

--- webapp.py
+++ webapp.py
@@ -384,7 +384,7 @@ def member_profile():
         member = collection_users.find_one({'_id': request.json['user_id']})
         queried_spaces = []
         for spaces in member['joined']:
-            space = collection_spaces.find_one({'_id': spaces})
+            space = collection_spaces.find_one({'_id': ObjectId(spaces)})
             queried_spaces.append(space["pictures"])
         user_data = {'name': member['name'], 'email': member['email'], 'picture': member['picture'], 'joined': queried_spaces}
         return Response(dumps(user_data), mimetype='application/json')
