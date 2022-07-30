d84115c878559d3a9ee5dc64a6c46e663bdd2738
Ramon Wang <ponmorw@gmail.com>
2022-07-30 15:08:02 -0700

    Vertical scrolling for spaces div

--- webapp.py
+++ webapp.py
@@ -359,12 +359,13 @@ def create_space():
         room_id = ObjectId()
         email_room_id = ObjectId()
         section_id = ObjectId()
-        rooms = [{'_id': room_id, 'space': str(space_id), 'section': str(section_id), 'name': 'general', 'order': 1}, {'_id': email_room_id, 'space': str(space_id), 'section': 'special', 'name': 'Email', 'order': 1}]
+        room = {'_id': room_id, 'space': str(space_id), 'section': str(section_id), 'name': 'general', 'order': 1}
+        special_rooms = {'_id': email_room_id, 'space': str(space_id), 'section': 'special', 'name': 'Email', 'order': 1}
         section = {'_id': section_id, 'space': str(space_id), 'name': 'discussion', 'order': 1}
         collection_spaces.insert_one({'_id': space_id, 'name': request.json['space_name'], 'picture': request.json['space_image'], 'admins': [session['unique_id']], 'members': [[session['unique_id'], session['users_name']]]})
-        collection_rooms.insert_many(rooms)
+        collection_rooms.insert_many([room, special_rooms])
         collection_sections.insert_one(section)
-        room_and_section = dumps([[rooms],[section]])
+        room_and_section = dumps([[room],[section]])
         
         joined = collection_users.find_one({"_id": session['unique_id']})['joined']
         joined.append(str(space_id))
