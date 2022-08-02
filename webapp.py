ced4e5a42ce085c9629cd77fde128427d44c7aa4
Ramon Wang <ponmorw@gmail.com>
2022-08-01 20:32:14 -0700

    Basic email template

--- webapp.py
+++ webapp.py
@@ -67,8 +67,8 @@ def send_email():
             message = MIMEMultipart('alternative')
             message['Subject'] = request.json['subject']
             message['From'] =  'Platform Test'
+            request.json['to'].append(session['users_email'])
             recipients = list(set(request.json['to']))
-            return request.json['to']
             text = (request.json['message'] + '<br>' +
             '--------------------------------------<br>' +
             session['users_name'] + '<br>' + 
