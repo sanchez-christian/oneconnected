2bf7c084be9d06f20b55878e7ff91336038244e7
Ramon Wang <ponmorw@gmail.com>
2022-08-01 20:11:10 -0700

    Basic email template

--- webapp.py
+++ webapp.py
@@ -68,7 +68,7 @@ def send_email():
             message['Subject'] = request.json['subject']
             message['From'] =  'Platform Test'
             recipients = list(set(request.json['to']))
-            text = (request.json['message'] + '<br>' +
+            text = (request.json['message'].replace('\n', '') + '<br>' +
             '--------------------------------------<br>' +
             session['users_name'] + '<br>' + 
             session['users_email'] + '<br>' +
