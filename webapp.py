a3936cce932cffbe32eacc02968de642d4de9ba0
Ramon Wang <ponmorw@gmail.com>
2022-08-04 20:37:22 -0700

    Added request class to app, mutates host url to use https

--- webapp.py
+++ webapp.py
@@ -48,11 +48,12 @@ collection_deleted = db['Deleted Messages']
 collection_logs = db['Logs']
 collection_emails = db['Emails']
 
+# Support SSL termination. Mutate the host_url within Flask to use https://
+# if the SSL was terminated.
+
 class ProxiedRequest(Request):
     def __init__(self, environ, populate_request=True, shallow=False):
         super(Request, self).__init__(environ, populate_request, shallow)
-        # Support SSL termination. Mutate the host_url within Flask to use https://
-        # if the SSL was terminated.
         x_forwarded_proto = self.headers.get('X-Forwarded-Proto')
         if  x_forwarded_proto == 'https':
             self.url = self.url.replace('http://', 'https://')
