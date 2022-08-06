ece8f016eace51f113a6c299217652f35f8a7d49
Ramon Wang <ponmorw@gmail.com>
2022-08-05 20:11:52 -0700

    Safari Mobile Tweak

--- webapp.py
+++ webapp.py
@@ -210,16 +210,16 @@ def get_google_provider_cfg():
 @app.route('/sbhs')
 @app.route('/sbhs/<space_id>')
 def render_main_page(space_id = None):
-    #if space_id is not None:
-    #    if 'logged' not in session or session['logged'] == False:
-    #        session['invite'] = space_id
-    #        return redirect(url_for('render_login'))
-    #if 'invite' in session:
-    #    space_id = session['invite']
-    #    session.pop('invite')
-    #    return redirect('https://sbhs-platform.herokuapp.com/sbhs/' + space_id) #TypeError: can only concatenate str (not "NoneType") to str
-    #if 'logged' not in session or session['logged'] == False:
-    #   return redirect(url_for('render_login'))
+    if space_id is not None:
+        if 'logged' not in session or session['logged'] == False:
+            session['invite'] = space_id
+            return redirect(url_for('render_login'))
+    if 'invite' in session:
+        space_id = session['invite']
+        session.pop('invite')
+        return redirect('https://sbhs-platform.herokuapp.com/sbhs/' + space_id) #TypeError: can only concatenate str (not "NoneType") to str
+    if 'logged' not in session or session['logged'] == False:
+       return redirect(url_for('render_login'))
     return render_template('index.html', name = session['users_name'], room = '1', picture = session['picture'], user_id = session['unique_id'])
 
 # When logout button is clicked, destroy session.
