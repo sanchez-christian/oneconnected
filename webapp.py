0d6af03e70f2ae0f6adc38bc3e7996ec2198be79
Ramon Wang <ponmorw@gmail.com>
2022-08-16 14:31:05 -0400

    Admin tables

--- webapp.py
+++ webapp.py
@@ -519,7 +519,7 @@ def server_logs():
             else:
                 index = 0
         if index != 0:
-            del logs[:index]
+            del logs[:index + 1]
         return Response(dumps(logs), mimetype='application/json')
 
 # When a room is clicked, make user join room
