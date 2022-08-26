d837d331aceda21c50c7e60067c9abb6421f7815
Ramon Wang <ponmorw@gmail.com>
2022-08-25 17:48:44 -0700

    Admin filters

--- webapp.py
+++ webapp.py
@@ -590,7 +590,7 @@ def server_logs():
         logs = None
         search = request.json['options'][0]
         if request.json['options'][1] == True and request.json['options'][2] == True:
-            logs = dumps(list(collection_logs.find({'$or': [{'doc.name': {'$regex': '.*' + search + '.*/i'}}]}).sort('_id', pymongo.DESCENDING).skip(int(request.json['i'])).limit(500)))
+            logs = dumps(list(collection_logs.find({'$or': [{'doc.name': {'$regex': '.*' + search + '.*/'}}]}).sort('_id', pymongo.DESCENDING).skip(int(request.json['i'])).limit(500)))
         elif request.json['options'][1] == True:
             logs = dumps(list(collection_logs.find({'doc.action': 'reported message'}).sort('_id', pymongo.DESCENDING).skip(int(request.json['i'])).limit(500)))
         elif request.json['options'][2] == True:
