c103d2dcef621f1686cd538ebdc14b77b4204289
Ramon Wang <ponmorw@gmail.com>
2022-08-25 18:18:14 -0700

    Admin filters

--- webapp.py
+++ webapp.py
@@ -588,7 +588,7 @@ def server_logs():
         return 'expired', 200
     if request.method == 'POST' and session['admin']:
         logs = None
-        search = '.*' + request.json['options'][0] + '.*/'
+        search = '.*' + request.json['options'][0] + '.*'
         if request.json['options'][1] == True and request.json['options'][2] == True:
             logs = dumps(list(collection_logs.find({'$or': [{'name': {'$regex': search}}, {'email': {'$regex': search}}, {'by': {'$regex': search}}, {'by_email': {'$regex': search}}, {'in': {'$regex': search}}, {'by': {'$regex': search}}]}).sort('_id', pymongo.DESCENDING).skip(int(request.json['i'])).limit(500)))
         elif request.json['options'][1] == True:
