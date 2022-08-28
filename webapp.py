7c7c4c80b7b7c1e6355dae226852e689c370bb9c
Ramon Wang <ponmorw@gmail.com>
2022-08-27 18:44:20 -0700

    8 new modals

--- webapp.py
+++ webapp.py
@@ -637,14 +637,14 @@ def edit_space_profile():
     if session_expired() or banned():
         return 'expired', 200
     if session['admin'] or space_admin():
-        space_picture = request.json['space_picture']
+        space_picture = request.json['space_picture'].strip()
         try:
             if not requests.head(space_picture).headers["content-type"] in ("image/png", "image/jpeg", "image/jpg", "image/gif", "image/avif", "image/webp", "image/svg") or int(requests.get(space_picture, stream = True).headers['Content-length']) > 6000000:
                 space_picture = '/static/images/Space.jpeg'
         except:
             space_picture = '/static/images/Space.jpeg'
-        collection_spaces.find_one_and_update({'_id': ObjectId(session['current_space'])}, {'$set': {'name': request.json['space_name'][:200], 'picture': space_picture, 'description': request.json['space_description'][:200]}})
-        return Response(dumps({'space_name': request.json['space_name'][:200], 'space_picture': space_picture, 'space_description': request.json['space_description'][:200]}), mimetype='application/json')
+        collection_spaces.find_one_and_update({'_id': ObjectId(session['current_space'])}, {'$set': {'name': request.json['space_name'][:200].strip(), 'picture': space_picture, 'description': request.json['space_description'][:200].strip()}})
+        return Response(dumps({'space_name': request.json['space_name'][:200].strip(), 'space_picture': space_picture, 'space_description': request.json['space_description'][:200].strip()}), mimetype='application/json')
     return Response(dumps({'success': 'false'}), mimetype='application/json')
 
 # When a room is clicked, make user join room
