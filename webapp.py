56a0c22506ed187bdf61ab2bb59df32d3be00b8c
Ramon Wang <ponmorw@gmail.com>
2022-08-27 17:41:19 -0700

    8 new modals

--- webapp.py
+++ webapp.py
@@ -623,11 +623,10 @@ def change_user_status():
         return 'expired', 200
     if request.method == 'POST' and session['admin']:
         if request.json['status'] in {'user', 'admin'}:
+            collection_users.find_one_and_update({"_id": request.json['user_id']}, {'$set': {'status': request.json['status']}})
             if request.json['user_id'] == session['unique_id'] and request.json['status'] == 'user':
-                session['admin'] = False
                 session.clear()
                 return Response(dumps({'success': 'self'}), mimetype='application/json')
-            collection_users.find_one_and_update({"_id": request.json['user_id']}, {'$set': {'status': request.json['status']}})
             return Response(dumps({'success': 'true'}), mimetype='application/json')
         elif request.json['status'] == 'banned' and collection_users.find({'_id': request.json['user_id']})['status'] not in {'admin', 'owner'}:
             collection_users.find_one_and_update({"_id": request.json['user_id']}, {'$set': {'status': 'banned'}})
