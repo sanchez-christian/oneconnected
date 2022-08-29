3dabf68f0d28f2ffa175da1831faf2d84f762cd8
Ramon Wang <ponmorw@gmail.com>
2022-08-29 15:27:39 -0700

    Space settings templating

--- webapp.py
+++ webapp.py
@@ -660,7 +660,7 @@ def change_user_status():
 
 @app.route('/edit_space_profile', methods=['POST'])
 def edit_space_profile():
-    collection_spaces.update({}, {'$set': {'invite_only': False}})
+    collection_spaces.update_many({}, {'$set': {'invite_only': False}})
     if session_expired() or banned():
         return 'expired', 200
     if session['admin'] or space_admin():
