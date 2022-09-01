551ff9a06c10e283e822fe70504321568973e9ce
Ramon Wang <ponmorw@gmail.com>
2022-09-01 15:23:52 -0700

    Admin logs space link

--- webapp.py
+++ webapp.py
@@ -520,7 +520,7 @@ def join_space():
         return Response({'exists': False}, mimetype='application/json')
     joined = collection_users.find_one({"_id": session['unique_id']})['joined']
     if request.json['space_id'] not in joined:
-        if space['invite_only'] and str(space['_id']) != session['code'] and not session['admin']:
+        if space['invite_only'] and request.json['space_id'] != session['code'] and not session['admin']:
             return Response({'invite_only': True}, mimetype='application/json')
         joined.append(request.json['space_id'])
         collection_users.find_one_and_update({"_id": session['unique_id']}, {'$set': {'joined': joined}})
