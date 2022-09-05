c5b7140bce8644b0c7f50453d8b10a4b2d2a221a
Ramon Wang <ponmorw@gmail.com>
2022-09-04 21:31:00 -0700

    Admin logs space link

--- webapp.py
+++ webapp.py
@@ -671,7 +671,7 @@ def change_user_status():
         user = collection_users.find_one({'_id': request.json['user_id']})
         space = collection_spaces.find_one({'_id': ObjectId(session['current_space'])})
         if request.json['status'] == 'banned':
-            collection_spaces.update_one({"_id": ObjectId(session['current_space'])}, { "$pull": {"members": {'$in': request.json['user_id']}}})
+            collection_spaces.update_one({"_id": ObjectId(session['current_space'])}, { "$pull": {"members": {'$in': [request.json['user_id']]}}})
             collection_spaces.find_one_and_update({"_id": ObjectId(request.json['current_space'])}, {'$push': {'members': [request.json['user_id'], user['name'], True]}})
             return Response(dumps({'success': 'true'}), mimetype='application/json')
     return Response(dumps({'success': 'false'}), mimetype='application/json')
