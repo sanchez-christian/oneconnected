7b9445ae4b2112dfa27571c4019e12b180faceef
Ramon Wang <ponmorw@gmail.com>
2022-09-07 18:03:29 -0700

    hello

--- webapp.py
+++ webapp.py
@@ -283,8 +283,8 @@ def render_space():
         return 'expired', 200
     if request.method == 'POST': # quicken query by querying ('$in') members with array of ObjectIds from members array...
         space = collection_spaces.find_one({'_id': ObjectId(request.json['space_id'])})
-        members = collection_users.find({'_id': {'$in': [i[0] for i in space['members']]}})
-        ban = collection_users.find({'_id': {'$in': space['banned']}})
+        members = list(collection_users.find({'_id': {'$in': [i[0] for i in space['members'] + space['banned']]}}))
+        #members += collection_users.find({'_id': {'$in': space['banned']}})
         rooms_and_sections = dumps([list(collection_rooms.find({'space': request.json['space_id']}).sort('order', 1)), list(collection_sections.find({'space': request.json['space_id']}).sort('order', 1)), list(members), list(collection_spaces.find({'_id': ObjectId(request.json['space_id'])}).limit(1)), list(collection_invites.find({'space': request.json['space_id']}))])
         if session['unique_id'] in space['admins'] or session['admin']:
             session['current_space'] = request.json['space_id']
