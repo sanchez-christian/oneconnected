1ee685215be7d408bc413bbb5e1f591f03c734e4
sanchez-christian <sanchezchristian.us@gmail.com>
2022-10-17 13:56:49 -0700

    Deployment

--- webapp.py
+++ webapp.py
@@ -667,7 +667,7 @@ def approve_space():
     if session_expired() or banned():
         return 'expired', 200
     if request.method == 'POST':
-        collection_spaceRequests.insert_one({'by': session['users_name'], 'user_id': session['unique_id'], 'email': session['users_email'], 'space_name': requests.json['space_name'], 'space_image': requests.json['space_iamge'], 'space_description': request.json['space_description']})
+        collection_spaceRequests.insert_one({'by': session['users_name'], 'user_id': session['unique_id'], 'email': session['users_email'], 'space_name': requests.json['space_name'], 'space_image': requests.json['space_iamge'], 'space_description': request.json['space_description'], 'status': 'unapproved'})
         return Response(dumps({'success': 'true'}), mimetype='application/json')
     session['logged'] = False
     session.clear()
