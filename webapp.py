4f5cb810de0d4c7518531d6e4a10ab5a00577aba
sanchez-christian <sanchezchristian.us@gmail.com>
2022-10-23 14:37:11 -0700

    Deployment

--- webapp.py
+++ webapp.py
@@ -709,7 +709,7 @@ def confirm_approve_space():
     if session_expired() or banned():
         return 'expired', 200
     if request.method == 'POST':
-        space_data = collection_spaceRequests.find_one({'_id': request.json['request_id']})
+        space_data = collection_spaceRequests.find_one({'_id': ObjectId(request.json['request_id'])})
         return Response(dumps(space_data), mimetype='application/json')
     session['logged'] = False
     session.clear()
